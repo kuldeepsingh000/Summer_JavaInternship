515412bc6fc2a1768c5083110749cc2263c7dc6f
KULDEEP SINGH <KULDEEP SINGH@DESKTOP-2JCQA5Q>
2020-05-29 00:20:24 +0530

    Done

--- JavaInternshipClg/src/javaStringsLevel1/Q23.java
+++ JavaInternshipClg/src/javaStringsLevel1/Q23.java
@@ -1,10 +1,20 @@
 package javaStringsLevel1;
 
+import java.util.Scanner;
+
+//23.	WAP to accept roll no, name of 5 students and display them 
+// back on the screen in tabular form.
+
 public class Q23 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s = sc.nextLine();
+		
+		
+		
 	}
 
 }
