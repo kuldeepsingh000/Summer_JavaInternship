5b5ab0059ca7fe7dce6a4f71bfc8706b9f3c607e
KULDEEP SINGH <KULDEEP SINGH@DESKTOP-2JCQA5Q>
2020-06-21 00:36:28 +0530

    Use of batch processing in jdbc

---
+++ JavaInternshipClg/src/jdbcPractical/DemoData.java
@@ -0,0 +1,34 @@
+package jdbcPractical;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DemoData {
+
+	public static void main(String[] args) throws Exception {
+		
+//		Demo databqse.
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@Localhost:1521:xe", "system", "1234");
+
+		Statement st = con.createStatement();
+		st.addBatch("insert into demo values(1, 'A', 65)");
+		st.addBatch("insert into demo values(2, 'B', 65)");
+		st.addBatch("insert into demo values(3, 'C', 66)");
+		st.addBatch("insert into demo values(4, 'D', 67)");
+		st.addBatch("insert into demo values(5, 'E', 68)");
+		
+		int[] i = st.executeBatch();
+		
+		for(int ii : i)
+		{
+			System.out.println(ii);
+		}
+		
+		con.close();
+		st.close();
+	}
+
+}
